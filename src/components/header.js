e2679b6f5a953c7bb5f44a4afae6d7268361af81
Avery-Dante Hinds <ahinds@parfunding.com>
2019-07-15 17:50:40 -0400

    started HempUp test site

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#FFFFFF`,
       marginBottom: `1.45rem`,
     }}
   >
