72744010a2de38351766fa80dca2586a56b17e18
Kathleenmont <kathleen.m.montesano@gmail.com>
2019-12-12 10:31:15 -0500

    start cart page

--- src/pages/store/cart.js
+++ src/pages/store/cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../../components/layout'
+// import Cart from '../../components/cart/cart'
 
 
 class Cart extends React.Component {
@@ -11,7 +12,7 @@ class Cart extends React.Component {
 
             <h1>Shoppin Cart</h1>
             <p>This is your Chopin Cart!!</p>
-            
+            {/* <Cart /> */}
             </Layout>
             </>
         )
@@ -19,3 +20,6 @@ class Cart extends React.Component {
 }
 
 export default Cart
+
+
+
