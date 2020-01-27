bc5f9772eb01c512a4f0d18296c09c8eb3b56c6b
thelovesmith <averydante9@gmail.com>
2020-01-27 17:37:08 -0500

    editing Cart Button for clients

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,27 +1,39 @@
-import React, { useContext, useEffect, useCallback,  useState, useRef } from "react"
+import React, {
+  useContext,
+  useEffect,
+  useCallback,
+  useState,
+  useRef,
+} from "react"
 import StoreContext from "../../context/globalcontext"
 import LineItem from "./lineItem"
-import {Button, Wrapper, ItemWrap, CheckoutWrap, Empty, P, H2} from "./cart.css"
+import {
+  Button,
+  Wrapper,
+  ItemWrap,
+  CheckoutWrap,
+  Empty,
+  P,
+  H2,
+} from "./cart.css"
 import { GlobalDispatchContext } from "../../provider/ContextProvider"
 import { GlobalStateContext } from "../../provider/ContextProvider"
+import { Link } from "gatsby"
 
-const Cart = (props) => {
-
+const Cart = props => {
   const dispatch = useContext(GlobalDispatchContext)
   const state = useContext(GlobalStateContext)
-  const [totalCart, setTotalCart] = useState();
+  const [totalCart, setTotalCart] = useState()
   const getSearchResults = () => {
     console.log(state.numInCart, "HERE I AM")
   }
 
-
   const {
     store: { checkout },
   } = useContext(StoreContext)
-  
 
   const handleCheckout = () => {
-    window.open(checkout.webUrl)
+    // window.open(checkout.webUrl)
     console.log(props)
   }
 
@@ -29,48 +41,50 @@ const Cart = (props) => {
     return <LineItem key={line_item.id.toString()} line_item={line_item} />
   })
 
-
   useEffect(() => {
     let array = []
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
     console.log(checkout)
     for (let i = 0; i < checkout.lineItems.length; i++) {
-        array.push(checkout.lineItems[i].quantity)
-        console.log(array,"array")
+      array.push(checkout.lineItems[i].quantity)
+      console.log(array, "array")
     }
-     let total = array.reduce(
-      ( accumulator, currentValue ) => accumulator + currentValue,
+    let total = array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
       0
     )
- 
+
     console.log(totalCart)
- 
+
     dispatch({ type: "SET_NUM", numInCart: total })
-  
+
     console.log(total, "final")
 
-getSearchResults()
-    
+    getSearchResults()
   }, [checkout.lineItems.length])
 
   return (
     <Wrapper>
-       <ItemWrap>
-      {checkout.subtotalPrice > 0 ?   line_items : <Empty>Your Cart is Empty</Empty>}
-     
-    
+      <ItemWrap>
+        {checkout.subtotalPrice > 0 ? (
+          line_items
+        ) : (
+          <Empty>Your Cart is Empty</Empty>
+        )}
       </ItemWrap>
       <CheckoutWrap>
-      {/* <h2>Subtotal</h2>
+        {/* <h2>Subtotal</h2>
       <p>$ {checkout.subtotalPrice}</p>
       <br /> */}
-      {/* <h2>Taxes</h2>
+        {/* <h2>Taxes</h2>
       <p>$ {checkout.totalTax}</p>
       <br /> */}
-      <H2>Total</H2>
-      <P>$ {checkout.totalPrice}</P>
-      <br />
-      <Button onClick={handleCheckout}>Check out</Button>
+        <H2>Total</H2>
+        <P>$ {checkout.totalPrice}</P>
+        <br />
+        <Button onClick={handleCheckout}>
+          <Link to="/store/cart">Check out</Link>
+        </Button>
       </CheckoutWrap>
     </Wrapper>
   )
