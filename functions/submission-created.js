9b81619e1c6d5b85593dcc121759b9317c600f02
thelovesmith <averydante9@gmail.com>
2020-02-10 17:23:23 -0500

    Added Function for newsletter subscription

---
+++ functions/submission-created.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const fetch = require("node-fetch")
+const { EMAIL_TOKEN } = process.env.GATSBY_BUTTONWOOD_KEY
+exports.handler = async event => {
+  const email = JSON.parse(event.body).payload.email
+  const formName = JSON.parse(event.body).payload.form_name
+  console.log(`Recieved a submission: ${email}`)
+  console.log(`FORM NAME IS: ${formName}`)
+  if (formName === "subscribe") {
+    return fetch("https://api.buttondown.email/v1/subscribers", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${EMAIL_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(`Submitted to Buttondown:\n ${data}`)
+      })
+      .catch(error => ({ statusCode: 422, body: String(error) }))
+  }
+}
